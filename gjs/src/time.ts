4e2fefe8c0174f6d8b8c9942b57bd89740a29f77
Aylur <k.demeter@protonmail.com>
2024-06-19 02:29:01 +0200

    fix(gjs): interval types

--- gjs/src/time.ts
+++ gjs/src/time.ts
@@ -1,13 +1,13 @@
 import { Astal } from "./imports.js"
 
-export function interval(interval: number, callback: () => void) {
-    return Astal.Time.interval(interval, callback)
+export function interval(interval: number, callback?: () => void) {
+    return Astal.Time.interval(interval, callback || null)
 }
 
-export function timeout(timeout: number, callback: () => void) {
-    return Astal.Time.timeout(timeout, callback)
+export function timeout(timeout: number, callback?: () => void) {
+    return Astal.Time.timeout(timeout, callback || null)
 }
 
-export function idle(callback: () => void) {
-    return Astal.Time.idle(callback)
+export function idle(callback?: () => void) {
+    return Astal.Time.idle(callback || null)
 }
