22132efcdeb08569cc27ca5414a0bb47511872d6
Aylur <k.demeter@protonmail.com>
2024-07-26 20:22:47 +0200

    fix: time functions
    dont explicitly return callback's return
    closure in vala expects void

--- gjs/src/time.ts
+++ gjs/src/time.ts
@@ -1,13 +1,13 @@
 import { Astal } from "./imports.js"
 
 export function interval(interval: number, callback?: () => void) {
-    return Astal.Time.interval(interval, callback || null)
+    return Astal.Time.interval(interval, () => void callback?.())
 }
 
 export function timeout(timeout: number, callback?: () => void) {
-    return Astal.Time.timeout(timeout, callback || null)
+    return Astal.Time.timeout(timeout, () => void callback?.())
 }
 
 export function idle(callback?: () => void) {
-    return Astal.Time.idle(callback || null)
+    return Astal.Time.idle(() => void callback?.())
 }
