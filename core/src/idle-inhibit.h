6916d59373948c8bc23cbb198795fe4608a73ba3
kotontrion <kotontrion@tutanota.de>
2024-09-27 08:47:32 +0200

    core: idle inhibitor fix code style

--- core/src/idle-inhibit.h
+++ core/src/idle-inhibit.h
@@ -3,6 +3,7 @@
 
 #include <glib-object.h>
 #include <gtk/gtk.h>
+
 #include "idle-inhibit-unstable-v1-client.h"
 
 G_BEGIN_DECLS
@@ -14,7 +15,7 @@ G_DECLARE_FINAL_TYPE(AstalInhibitManager, astal_inhibit_manager, ASTAL, INHIBIT_
 typedef struct zwp_idle_inhibitor_v1 AstalInhibitor;
 
 AstalInhibitManager* astal_inhibit_manager_get_default();
-AstalInhibitor* astal_inhibit_manager_inhibit(AstalInhibitManager *self, GtkWindow *window);
+AstalInhibitor* astal_inhibit_manager_inhibit(AstalInhibitManager* self, GtkWindow* window);
 
 G_END_DECLS
 
