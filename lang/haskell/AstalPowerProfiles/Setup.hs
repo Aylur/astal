7cee58f300732bb069a138bf6e1df0a9ea74a468
kotontrion <kotontrion@tutanota.de>
2024-09-30 16:16:50 +0200

    haskell: add more libs

---
+++ lang/haskell/AstalPowerProfiles/Setup.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.GI.CodeGen.CabalHooks (setupBinding, TaggedOverride(..))
+
+import qualified GI.GObject.Config as GObject
+
+
+main :: IO ()
+main = setupBinding name version pkgName pkgVersion verbose overridesFile inheritedOverrides outputDir
+  where name = "AstalPowerProfiles"
+        version = "0.1"
+        pkgName = "gi-astal-power-profiles"
+        pkgVersion = "0.1.0"
+        overridesFile = Nothing
+        verbose = False
+        outputDir = Nothing
+        inheritedOverrides = [TaggedOverride "inherited:GObject" GObject.overrides]
---
+++ lang/haskell/AstalTray/Setup.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.GI.CodeGen.CabalHooks (setupBinding, TaggedOverride(..))
+
+import qualified GI.GObject.Config as GObject
+import qualified GI.GdkPixbuf.Config as GdkPixbuf
+import qualified GI.Gio.Config as Gio
+import qualified GI.Gtk.Config as Gtk
+
+
+main :: IO ()
+main = setupBinding name version pkgName pkgVersion verbose overridesFile inheritedOverrides outputDir
+  where name = "AstalTray"
+        version = "0.1"
+        pkgName = "gi-astal-tray"
+        pkgVersion = "0.1.0"
+        overridesFile = Nothing
+        verbose = False
+        outputDir = Nothing
+        inheritedOverrides = [TaggedOverride "inherited:GObject" GObject.overrides, TaggedOverride "inherited:GdkPixbuf" GdkPixbuf.overrides, TaggedOverride "inherited:Gio" Gio.overrides, TaggedOverride "inherited:Gtk" Gtk.overrides]
