484c3e7a9ca4ad20e4b872e9aadc1a70a3542ad4
kotontrion <kotontrion@tutanota.de>
2024-10-23 10:45:26 +0200

    haskell: update bindings

--- lang/haskell/Astal/ChangeLog.md
+++ lang/haskell/Astal/ChangeLog.md
@@ -1,3 +1,6 @@
+### 3.0.0
+split into io and gtk3 specific packages
+
 ### 0.1.0
 
 initial release
--- lang/haskell/Astal/Setup.hs
+++ lang/haskell/Astal/Setup.hs
@@ -2,6 +2,7 @@
 
 import Data.GI.CodeGen.CabalHooks (setupBinding, TaggedOverride(..))
 
+import qualified GI.AstalIO.Config as AstalIO
 import qualified GI.GObject.Config as GObject
 import qualified GI.Gdk.Config as Gdk
 import qualified GI.GdkPixbuf.Config as GdkPixbuf
@@ -12,10 +13,10 @@ import qualified GI.Gtk.Config as Gtk
 main :: IO ()
 main = setupBinding name version pkgName pkgVersion verbose overridesFile inheritedOverrides outputDir
   where name = "Astal"
-        version = "0.1"
+        version = "3.0"
         pkgName = "gi-astal"
-        pkgVersion = "0.1.0"
+        pkgVersion = "3.0.0"
         overridesFile = Nothing
         verbose = False
         outputDir = Nothing
-        inheritedOverrides = [TaggedOverride "inherited:GObject" GObject.overrides, TaggedOverride "inherited:Gdk" Gdk.overrides, TaggedOverride "inherited:GdkPixbuf" GdkPixbuf.overrides, TaggedOverride "inherited:Gio" Gio.overrides, TaggedOverride "inherited:Gtk" Gtk.overrides]
+        inheritedOverrides = [TaggedOverride "inherited:AstalIO" AstalIO.overrides, TaggedOverride "inherited:GObject" GObject.overrides, TaggedOverride "inherited:Gdk" Gdk.overrides, TaggedOverride "inherited:GdkPixbuf" GdkPixbuf.overrides, TaggedOverride "inherited:Gio" Gio.overrides, TaggedOverride "inherited:Gtk" Gtk.overrides]
