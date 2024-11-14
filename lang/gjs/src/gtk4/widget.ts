60bd2d628432b69322e2423ff302317e5d82a7e3
Valentyn <valentyn.fuchedzhy@gmail.com>
2024-11-14 00:25:56 -0800

    use childrenSetter for window

--- lang/gjs/src/gtk4/widget.ts
+++ lang/gjs/src/gtk4/widget.ts
@@ -3,7 +3,16 @@ import astalify, { BindableChild } from "./astalify"
 import GObject from "gi://GObject"
 
 export type WindowProps = ConstructProps<Window, Astal.Window.ConstructorProps>
-export class Window extends astalify(Astal.Window) {
+export class Window extends astalify(
+    Astal.Window,
+    (children, self) => {
+        if (children.length != 1) {
+            console.error(`Window can have only 1 child, attempted to set ${children.length}`)
+        }
+        if (children.length > 0)
+            self.set_child(children[0])
+    },
+) {
     static { GObject.registerClass({ GTypeName: "Window" }, this) }
     constructor(props?: WindowProps, child?: BindableChild) { super({ child, ...props } as any) }
 }
