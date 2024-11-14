9eff88e6f7cf230c6c0045a23df1e9544732bbc5
Valentyn <valentyn.fuchedzhy@gmail.com>
2024-11-13 22:43:20 -0800

    basic gtk4

--- lang/gjs/src/gtk4/widget.ts
+++ lang/gjs/src/gtk4/widget.ts
@@ -1,1 +1,9 @@
-// TODO:
+import { Astal, ConstructProps } from "../gtk3"
+import astalify, { BindableChild } from "./astalify"
+import GObject from "gi://GObject"
+
+export type WindowProps = ConstructProps<Window, Astal.Window.ConstructorProps>
+export class Window extends astalify(Astal.Window) {
+    static { GObject.registerClass({ GTypeName: "Window" }, this) }
+    constructor(props?: WindowProps, child?: BindableChild) { super({ child, ...props } as any) }
+}
