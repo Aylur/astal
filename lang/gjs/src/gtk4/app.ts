70ed5584541e4fe2161428d1983ce12e77978291
Aylur <k.demeter@protonmail.com>
2025-01-06 00:37:31 +0100

    fix(gjs): app.launch

--- lang/gjs/src/gtk4/app.ts
+++ lang/gjs/src/gtk4/app.ts
@@ -4,6 +4,10 @@ import { mkApp } from "../_app"
 
 Gtk.init()
 
+// stop this from leaking into subprocesses
+// and gio launch invocations
+GLib.unsetenv("LD_PRELOAD")
+
 // users might want to use Adwaita in which case it has to be initialized
 // it might be common pitfall to forget it because `App` is not `Adw.Application`
 await import("gi://Adw?version=1")
