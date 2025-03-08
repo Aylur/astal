c890d3aa0eff7284d4382f230aa6fe3851e9e978
matt1432 <matt@nelim.org>
2025-03-08 16:56:06 -0500

    feat(nix): overlay nixpkgs to expose packages

--- flake.nix
+++ flake.nix
@@ -11,47 +11,54 @@
   }: let
     perSystem = attrs:
       nixpkgs.lib.genAttrs (import systems) (system:
-        attrs (import nixpkgs {inherit system;}));
+        attrs (import nixpkgs {
+          inherit system;
+          overlays = [self.overlays.default];
+        }));
   in {
     lib = {
       mkLuaPackage = import ./nix/lua.nix self;
     };
 
-    packages = perSystem (pkgs: let
-      mkPkg = src:
-        import src {
-          inherit self pkgs;
-          mkAstalPkg = import ./nix/mkAstalPkg.nix pkgs;
+    packages = perSystem (pkgs: pkgs.astal // {default = pkgs.astal.io;});
+
+    overlays = {
+      astal = final: prev: {
+        astal = let
+          mkPkg = src:
+            import src {
+              pkgs = final;
+              mkAstalPkg = import ./nix/mkAstalPkg.nix final;
+            };
+        in {
+          docs = import ./docs final;
+
+          io = mkPkg ./lib/astal/io;
+          astal3 = mkPkg ./lib/astal/gtk3;
+          astal4 = mkPkg ./lib/astal/gtk4;
+          apps = mkPkg ./lib/apps;
+          auth = mkPkg ./lib/auth;
+          battery = mkPkg ./lib/battery;
+          bluetooth = mkPkg ./lib/bluetooth;
+          cava = mkPkg ./lib/cava;
+          greet = mkPkg ./lib/greet;
+          hyprland = mkPkg ./lib/hyprland;
+          mpris = mkPkg ./lib/mpris;
+          network = mkPkg ./lib/network;
+          notifd = mkPkg ./lib/notifd;
+          powerprofiles = mkPkg ./lib/powerprofiles;
+          river = mkPkg ./lib/river;
+          tray = mkPkg ./lib/tray;
+          wireplumber = mkPkg ./lib/wireplumber;
+
+          gjs = import ./lang/gjs final;
         };
-    in {
-      default = self.packages.${pkgs.system}.io;
-      docs = import ./docs {inherit self pkgs;};
-
-      io = mkPkg ./lib/astal/io;
-      astal3 = mkPkg ./lib/astal/gtk3;
-      astal4 = mkPkg ./lib/astal/gtk4;
-      apps = mkPkg ./lib/apps;
-      auth = mkPkg ./lib/auth;
-      battery = mkPkg ./lib/battery;
-      bluetooth = mkPkg ./lib/bluetooth;
-      cava = mkPkg ./lib/cava;
-      greet = mkPkg ./lib/greet;
-      hyprland = mkPkg ./lib/hyprland;
-      mpris = mkPkg ./lib/mpris;
-      network = mkPkg ./lib/network;
-      notifd = mkPkg ./lib/notifd;
-      powerprofiles = mkPkg ./lib/powerprofiles;
-      river = mkPkg ./lib/river;
-      tray = mkPkg ./lib/tray;
-      wireplumber = mkPkg ./lib/wireplumber;
-
-      gjs = import ./lang/gjs {inherit self pkgs;};
-    });
+      };
+      default = self.overlays.astal;
+    };
 
     devShells = perSystem (pkgs:
-      import ./nix/devshell.nix {
-        inherit pkgs self;
-      });
+      import ./nix/devshell.nix pkgs);
 
     formatter = perSystem (pkgs: pkgs.alejandra);
   };
