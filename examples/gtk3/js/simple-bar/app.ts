b948b3726d2b01ba9dff0baf1b8f5f88e4d38e1e
Jas Singh <jaskiratpal.singh@outlook.com>
2025-04-05 02:05:16 -0700

    combine notification into bar

--- examples/gtk3/js/simple-bar/app.ts
+++ examples/gtk3/js/simple-bar/app.ts
@@ -1,13 +1,17 @@
-import { App } from "astal/gtk3"
-import style from "./style.scss"
-import Bar from "./widget/Bar"
+import { App } from "astal/gtk3";
+import style from "./style.scss";
+import Bar from "./widget/Bar";
+import NotificationPopups from "./notifications/NotificationPopups";
 
 App.start({
-    css: style,
-    instanceName: "js",
-    requestHandler(request, res) {
-        print(request)
-        res("ok")
-    },
-    main: () => App.get_monitors().map(Bar),
-})
+  css: style,
+  instanceName: "js",
+  requestHandler(request, res) {
+    print(request);
+    res("ok");
+  },
+  main: () => {
+    App.get_monitors().map(Bar);
+    App.get_monitors().map(NotificationPopups);
+  },
+});
