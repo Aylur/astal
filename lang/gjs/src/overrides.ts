4e455bb6e7f256742849322d31427b9defc95c16
kotontrion <kotontrion@tutanota.de>
2025-06-13 17:07:10 +0200

    fix(gjs): add more property overrides

--- lang/gjs/src/overrides.ts
+++ lang/gjs/src/overrides.ts
@@ -36,6 +36,10 @@ await suppress(import("gi://AstalBluetooth"), ({ Adapter, Bluetooth, Device }) =
     patch(Device.prototype, "uuids")
 })
 
+await suppress(import("gi://AstalCava"), ({ Cava }) => {
+    patch(Cava.prototype, "values")
+})
+
 await suppress(import("gi://AstalHyprland"), ({ Hyprland, Monitor, Workspace }) => {
     patch(Hyprland.prototype, "binds")
     patch(Hyprland.prototype, "monitors")
@@ -69,6 +73,14 @@ await suppress(import("gi://AstalPowerProfiles"), ({ PowerProfiles }) => {
     patch(PowerProfiles.prototype, "actions")
 })
 
+await suppress(import("gi://AstalRiver"), ({ River }) => {
+  patch(River.prototype, "outputs")
+})
+
+await suppress(import("gi://AstalTray"), ({ Tray }) => {
+  patch(Tray.prototype, "items")
+})
+
 await suppress(import("gi://AstalWp"), ({ Wp, Audio, Video, Node, Endpoint, Device }) => {
     patch(Wp.prototype, "nodes")
     patch(Wp.prototype, "devices")
@@ -86,4 +98,8 @@ await suppress(import("gi://AstalWp"), ({ Wp, Audio, Video, Node, Endpoint, Devi
     patch(Node.prototype, "channels")
     patch(Device.prototype, "profiles")
     patch(Device.prototype, "routes")
+    patch(Device.prototype, "input_routes")
+    patch(Device.prototype, "inputRoutes")
+    patch(Device.prototype, "output_routes")
+    patch(Device.prototype, "outputRoutes")
 })
