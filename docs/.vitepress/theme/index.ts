6fb7847f61cb1b7eed6c468f91816aa0e2a7d481
Aylur <k.demeter@protonmail.com>
2026-04-30 21:32:57 +0200

    docs: add analytics

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,16 @@
-import DefaultTheme from "vitepress/theme"
 import "../../vitepress.theme.css"
 import "devicon/devicon.min.css"
 import "font-logos/assets/font-logos.css"
 
-export default DefaultTheme
+import { inject } from "@vercel/analytics"
+import type { Theme } from "vitepress"
+import DefaultTheme from "vitepress/theme"
+
+const theme: Theme = {
+  extends: DefaultTheme,
+  enhanceApp() {
+    inject()
+  },
+}
+
+export default theme
