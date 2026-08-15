735aecafd62cc9145a3129dc6df59251053fa2c7
Aylur <k.demeter@protonmail.com>
2026-08-16 00:01:08 +0200

    nix: add astal-workspace derivation

---
+++ lib/workspace/default.nix
@@ -0,0 +1,24 @@
+{
+  mkAstalPkg,
+  pkgs,
+  self,
+}: let
+  astal = self.packages.${pkgs.stdenv.hostPlatform.system};
+in
+  mkAstalPkg {
+    pname = "astal-workspace";
+    src = ./.;
+    packages = [
+      astal.quarrel
+      astal.wl
+      pkgs.gtk4
+      pkgs.json-glib
+    ];
+
+    libname = "workspace";
+    name = "AstalWorkspace";
+    authors = "Mabi";
+    description = "Astal Wayland Workspace";
+    dependencies = ["AstalWl-0.1"];
+    repo-path = "astal/workspace";
+  }
